2b32a9ee4b4461ff75faa99ff517fef7aca059ac
muchbeer <gman2them@gmail.com>
2015-07-07 11:24:31 +0300

    The best sign in

---
+++ app/src/main/java/muchbeer/king/sokohurusign/AddNewItem.java
@@ -0,0 +1,16 @@
+package muchbeer.king.sokohurusign;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by muchbeer on 7/7/2015.
+ */
+public class AddNewItem extends ActionBarActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_additem);
+    }
+}
